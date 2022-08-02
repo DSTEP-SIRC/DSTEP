02fd4d4b2c33a76a6a4fb0f74d81030dd06719dc
DynamicAR <110189064+DynamicAR@users.noreply.github.com>
2022-08-02 12:30:24 +0300

    Delete DGNet directory

--- DGNet/models/mobilenet_v2/__init__.py
+++
@@ -1,3 +0,0 @@
-from __future__ import absolute_import
-
-from .mobilenet_v2_dg import *
--- DGNet/scripts/imagenet_e.sh
+++
@@ -1,4 +0,0 @@
-echo "Dynamic dual gating model $1 for ImageNet."
-
-time python main.py -d $2 -dset imagenet -a $1 \
-     --checkpoint $4 --gpu-id $3 --pretrained $5 -e
