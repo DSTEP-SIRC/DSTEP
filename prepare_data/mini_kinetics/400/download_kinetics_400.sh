0a973ba388edc44e8fe83c0a0f5b556525a138ae
DynamicAR <110189064+DynamicAR@users.noreply.github.com>
2022-08-02 12:30:48 +0300

    Delete prepare_data directory

--- prepare_data/mini_kinetics/400/download_kinetics_400.sh
+++
@@ -1,7 +0,0 @@
-mkdir /algo/CBI_inputs/Kinetics-400
-cd /algo/CBI_inputs/Kinetics-400
-while read one;
-do
-    echo $one
-    wget "$one"
-done < $1
--- prepare_data/mini_kinetics/400/extract_kinetics_400.sh
+++
@@ -1,5 +0,0 @@
-while read one;
-do
-    echo ${one##*/}
-    tar zxvf ${one##*/}
-done < $1
